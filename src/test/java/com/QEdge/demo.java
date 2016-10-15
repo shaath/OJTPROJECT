32286eff8beb31593172e8f90699bce031a5d067
sharath chandra <sharath chandra>
2016-10-15 10:58:00 +0530

    Fist Commit

---
+++ src/test/java/com/QEdge/demo.java
@@ -0,0 +1,11 @@
+package com.QEdge;
+
+public class demo {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to selenium");
+
+	}
+
+}
---
+++ src/test/java/com/QEdge/fflaunch.java
@@ -0,0 +1,15 @@
+package com.QEdge;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class fflaunch {
+	@Test
+	public void mm() 
+	{
+		WebDriver driver=new FirefoxDriver();
+
+	}
+
+}
